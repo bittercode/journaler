fa748ab770687bcc59946d5a936437f8656c14e1
JR Peck <bittercode@gmail.com>
2012-05-01 16:07:28 +0200

    Working on pulling it together - spreads handles spreadsheet stuff, txt2pdf will handle pdf conversion, if I can get it to work.

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
 
 import findfiles
+import txt2pdf
+
+def write_header(txtfile,header):
+    
+    with open(txtfile,'r+') as f:
+        content = f.read()
+        f.seek(0,0)
+        f.write(header.rstrip('\r\n') + '\n' + content)
 
 myfile = findfiles.FileReader()
 
-print myfile.find_text("/mnt/f002/jr")
+for txtfile in myfile.find_text("/mnt/f002/jr"):
+    #Get the appropriate spreadsheet
+    
+    #pdfout = txtfile[:-3] + "pdf"
+    #pdfconvert = txt2pdf.Txt2Pdf()
+    #pdfconvert.txtconvert(txtfile,pdfout)
---
+++ spreads.py
@@ -0,0 +1,32 @@
+from xlrd import open_workbook
+from xlwt import easyxf
+from xlutils.copy import copy
+
+class SpreadSheet(object):
+    
+    def last_empty(wkbook_name):
+        wb = xlrd.open_workbook(wkbook_name)
+
+        sh = wb.sheet_by_name('Journal')
+        
+        i=7
+        ccell='init'
+
+        while ccell:
+            ccell = sh.cell_value(i,6)
+            i += 1
+
+        return i-1
+    
+    def write_link(wkbook_name,cell_row):
+        rb = open_workbook(wkbook_name,formatting_info=True)
+        rs = rb.sheet_by_index(0)
+        wb = copy(rb)
+        ws = wb.get_sheet(0)
+
+        plain = easyxf('')
+        ws.write(22,2,'jrwashere',plain)
+
+        ws.write(22,4,2345)
+
+        wb.save(wkbook_name)
